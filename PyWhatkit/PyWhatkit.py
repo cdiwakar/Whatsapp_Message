26cae91d11b5a3e964068c1bf647921673e23c2b
cdiwakar <diwakar1807@gmail.com>
2023-01-21 16:31:41 +0530

    Whatsapp

---
+++ PyWhatkit/PyWhatkit.py
@@ -0,0 +1,9 @@
+import pywhatkit
+#Send Message to Number
+pywhatkit.sendwhatmsg('+919727759680','test',16,17,30)
+#Send Instant message
+pywhatkit.sendwhatmsg_instantly('+919727759680','instant msg',15)
+#Play on Youtube
+pywhatkit.playonyt('great learning')
+#Do Google Search
+pywhatkit.search('open chatgpt')
